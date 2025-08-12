775c4720852a0196650069dc98868de240193e58
arielHernandez-202402123 <202402123@umh.edu.hn>
2025-08-11 21:36:54 -0600

    Agregar archivos de proyecto.

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("****Bienvenido a la Calculadora de Sueldo****");
+            Console.WriteLine("Ingrese su nombre:");
+            string nombre = Console.ReadLine();
+        
+            Console.Write("Ingrese el Costo de hora trabajada: ");
+            double costoHora = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de horas trabajadas:");
+            double cantidadHoras = Convert.ToDouble(Console.ReadLine());
+
+            int totalDevengado = (int)(costoHora * cantidadHoras);
+            Console.WriteLine($"Hola {nombre}, el total devengado por {cantidadHoras} horas trabajadas a un costo de L{costoHora} por hora es: L{totalDevengado}");
+
+
+        }
+    }
+}
